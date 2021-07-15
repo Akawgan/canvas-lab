e298f953382cf5d09e87d5f7a93c56ddb8f82e46
Savien <akawgan@gmail.com>
2021-07-15 23:51:42 +0200

    Add dot movement

--- canvas.js
+++ canvas.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var x = 0;
   
     // Event handler to resize the canvas when the document view is changed
     window.addEventListener('resize', resizeCanvas, false);
@@ -15,10 +16,17 @@ var ctx = canvas.getContext('2d');
   
     function drawStuff() {
       // Do drawing here
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
         ctx.strokeStyle = "#000000";
-        ctx.arc(canvas.width / 2 - 5, 75, 5, 0, 2 * Math.PI);
+        ctx.arc(canvas.width / 2 - 5 + x, canvas.height / 2 - 5, 5, 0, 2 * Math.PI);
         ctx.fillStyle = "#000000";
         ctx.fill();
         ctx.stroke();
+
+        x += 1; // Move the dot to the right
+
+        
     }
+
+    setInterval(drawStuff, 6);
