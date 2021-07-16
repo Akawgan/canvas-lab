9a67513c0e5a9d9e7831b4f7790e5ed8b6858f42
Savien <akawgan@gmail.com>
2021-07-16 23:30:49 +0200

    Randomize the dots movement

--- canvas.js
+++ canvas.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var x = 0;
+var y = 0;
   
     // Event handler to resize the canvas when the document view is changed
     window.addEventListener('resize', resizeCanvas, false);
@@ -16,15 +17,24 @@ var x = 0;
   
     function drawStuff() {
       // Do drawing here
+
+      function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+
+        let vector2D = [getRandomInt(-1, 2), getRandomInt(-1, 2)]
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
         ctx.strokeStyle = "#000000";
-        ctx.arc(canvas.width / 2 - 5 + x, canvas.height / 2 - 5, 5, 0, 2 * Math.PI);
+        ctx.arc(canvas.width / 2 - 5 + x, canvas.height / 2 - 5 + y, 5, 0, 2 * Math.PI);
         ctx.fillStyle = "#000000";
         ctx.fill();
         ctx.stroke();
 
-        x += 1; // Move the dot to the right
+        x += vector2D[0];
+        y += vector2D[1];
 
         
     }
